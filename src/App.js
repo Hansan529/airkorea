7a1cd81e8ce3ff96932637883b9fd4e486a22a3d
hansan529 <hansan0529@gmail.com>
2024-01-27 22:44:27 +0900

    docs: emotion/styled 변경, Header 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,115 +1,12 @@
-import styled from 'styled-components';
+import styled from '@emotion/styled';
 import './App.css';
-
-const headerHeight = '90px';
+import Headers from './components/Header';
 
 const Select = styled.select`
   -o-appearance: none;
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none;
-`;
-
-const Header = styled.header`
-  display: flex;
-  position: relative;
-  width: 100%;
-  min-width: 1400px;
-  height: ${headerHeight};
-  z-index: 100;
-  background-color: #fff;
-  border-bottom: 1px solid #dcdcdc;
-`;
-
-const HeaderContainer = styled.div`
-  position: relative;
-  min-width: 1400px;
-  width: 1400px;
-  margin: 0 auto;
-`;
-
-const MainLogo = styled.h1`
-  position: absolute;
-  margin: 0;
-  top: 20px;
-  left: 0;
-
-  a {
-    display: block;
-
-    img {
-      vertical-align: middle;
-      border: 0;
-    }
-  }
-`;
-
-const GlobalNav = styled.div`
-  height: 100%;
-`;
-
-const NavUl = styled.ul`
-  display: flex;
-  justify-content: center;
-  gap: 58px;
-  margin: 0;
-  padding: 0;
-`;
-
-const NavLi = styled.li`
-  display: inline-block;
-  line-height: ${headerHeight};
-  list-style: none;
-
-  a {
-    text-decoration: none;
-    font-weight: 600;
-    font-size: 18px;
-    display: block;
-    &:visited {
-      color: #000;
-    }
-    &:hover {
-      color: #0f62cc;
-    }
-  }
-`;
-
-const Util = styled.div`
-  display: flex;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  right: 0;
-  gap: 20px;
-  align-items: center;
-`;
-
-const LanguageBtn = styled.a`
-  padding: 0 30px 0 0;
-  display: inline-block;
-  background: url('/img_bg_lang.png') no-repeat right center;
-  letter-spacing: -0.4px;
-  color: #0a0a0a;
-`;
-
-const UtilBtn = styled.button`
-  display: inline-block;
-  width: 25px;
-  height: 24px;
-  background: url(${(props) => props.bgImg}) no-repeat center;
-  position: relative;
-  border: none;
-
-  > span {
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    overflow: hidden;
-    border: 0;
-    padding: 0;
-    white-space: nowrap;
-  }
 `;
 
 const Main = styled.main``;
@@ -143,6 +40,7 @@ const MapUl = styled.ul`
     }
     a.on {
       background: #0f62cc;
+      color: #fff;
     }
   }
 `;
@@ -188,59 +86,15 @@ const Round = styled(Select)`
 function App() {
   return (
     <>
-      <Header>
-        <HeaderContainer>
-          <MainLogo>
-            <a href="/" title="메인페이지">
-              <img src={'/logo.png'} alt="logo" />
-            </a>
-          </MainLogo>
-          <GlobalNav>
-            <NavUl>
-              <NavLi>
-                <a href="/">에어코리아란</a>
-              </NavLi>
-              <NavLi>
-                <a href="/">실시간 자료조회</a>
-              </NavLi>
-              <NavLi>
-                <a href="/">대기정보 예보 / 경보</a>
-              </NavLi>
-              <NavLi>
-                <a href="/">통계정보</a>
-              </NavLi>
-              <NavLi>
-                <a href="/">배움터</a>
-              </NavLi>
-              <NavLi>
-                <a href="/">고객지원</a>
-              </NavLi>
-            </NavUl>
-          </GlobalNav>
-          <Util>
-            <LanguageBtn
-              href="/eng"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="영문 버전으로 새창 이동"
-            >
-              En
-            </LanguageBtn>
-            <UtilBtn bgImg={'img_bg_search.png'}>
-              <span>통합검색 열기</span>
-            </UtilBtn>
-            <UtilBtn bgImg={'img_bg_ham.png'}>
-              <span>메뉴 열기</span>
-            </UtilBtn>
-          </Util>
-        </HeaderContainer>
-      </Header>
+      <Headers />
       <Main>
         <FirstSection>
           <Map>
             <MapUl>
               <li>
-                <a href>실시간 대기정보</a>
+                <a className="on" href>
+                  실시간 대기정보
+                </a>
               </li>
               <li>
                 <a href>오늘/내일/모레 대기정보</a>
