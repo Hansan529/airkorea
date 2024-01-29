d326b5033af3d0f7860a013e11e655e25df250d2
hansan529 <hansan0529@gmail.com>
2024-01-29 23:35:19 +0900

    docs: 대기/기상 정보 레이아웃 추가

--- src/App.js
+++ src/App.js
@@ -31,14 +31,16 @@ const Select = styled.select`
 const Main = styled.main``;
 const Section = styled.section``;
 
+const Time = styled.span``;
+
 const FirstSection = styled(Section)`
   background: url('/img_main_bg.png') repeat-x 0 0;
 `;
 
+const MMLayout = styled.div``;
 const MMOptionLayout = styled.div`
   width: 710px;
 `;
-
 const MMOList = styled.ul`
   display: flex;
   margin: 0;
@@ -139,6 +141,10 @@ const MMWrapper = styled.div`
   }
 `;
 
+//
+
+const InfoContainer = styled.div``;
+
 function App() {
   return (
     <>
@@ -146,103 +152,126 @@ function App() {
       <Main>
         {/* 첫번째 색션 */}
         <FirstSection>
-          {/* Main Map 설정 */}
-          <MMOptionLayout>
-            <MMOList>
-              <li>
-                <a className="on" href>
-                  실시간 대기정보
-                </a>
-              </li>
-              <li>
-                <a href>오늘/내일/모레 대기정보</a>
-              </li>
-              <li>
-                <a href>실시간 기상정보</a>
-              </li>
-              <li>
-                <a href>오늘/내일/모레 기상정보</a>
-              </li>
-            </MMOList>
-            <MMOContainer>
-              <MMOSelectWrapper>
-                <MMOSelect bg width="188px">
-                  <option value="KHAI">통합대기환경지수(CAI)</option>
-                  <option value="10008">초미세먼지 (PM-2.5)</option>
-                  <option value="10007">미세먼지 (PM-10)</option>
-                  <option value="10003">오존(O₃)</option>
-                  <option value="10006">이산화질소(NO₂)</option>
-                  <option value="10002">일산화탄소 (CO)</option>
-                  <option value="10001">아황산가스 (SO₂)</option>
-                </MMOSelect>
-              </MMOSelectWrapper>
-              <MMOSelectWrapper>
-                <label for="area1" style={{ marginRight: '5px' }}>
-                  시/도
-                </label>
-                <MMOSelect bg id="area1" width="130px">
-                  <option value="">-전체-</option>
-                  <option value="02">서울특별시</option>
-                  <option value="051">부산광역시</option>
-                  <option value="053">대구광역시</option>
-                  <option value="032">인천광역시</option>
-                  <option value="062">광주광역시</option>
-                  <option value="042">대전광역시</option>
-                  <option value="052">울산광역시</option>
-                  <option value="031">경기도</option>
-                  <option value="033">강원특별자치도</option>
-                  <option value="043">충청북도</option>
-                  <option value="041">충청남도</option>
-                  <option value="063">전라북도</option>
-                  <option value="061">전라남도</option>
-                  <option value="044">세종특별자치시</option>
-                  <option value="054">경상북도</option>
-                  <option value="055">경상남도</option>
-                  <option value="064">제주특별자치도</option>
-                </MMOSelect>
-              </MMOSelectWrapper>
-            </MMOContainer>
-          </MMOptionLayout>
-          {/* Main Map 전국 지도 */}
-          <MMWrapper>
-            <svg
-              version="1.1"
-              id="map_svg_city"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              x="0px"
-              y="0px"
-              width="700px"
-              height="730px"
-              viewBox="0 0 700 730"
-              enable-background="new 0 0 700 730"
-              xmlS
-              pace="preserve"
-            >
-              <g>
-                <IncheonPath />
-                <SeoulPath />
-                <GyeonggiPath />
-                <GangwonPath />
-                <ChungbukPath />
-                <ChungnamPath />
-                <SejongPath />
-                <DaejeonPath />
-                <GyeongbukPath />
-                <GyeongnamPath />
-                <DaeguPath />
-                <UlsanPath />
-                <BusanPath />
-                <JeonbukPath />
-                <JeonnamPath />
-                <GwangjuPath />
-                <JejuPath />
-              </g>
-            </svg>
-            <span>
-              0000년 00월 00일 <strong>00시</strong>
-            </span>
-          </MMWrapper>
+          {/* 대기/기상 지도 정보 */}
+          <MMLayout>
+            {/* Main Map 설정 */}
+            <MMOptionLayout>
+              <MMOList>
+                <li>
+                  <a className="on" href>
+                    실시간 대기정보
+                  </a>
+                </li>
+                <li>
+                  <a href>오늘/내일/모레 대기정보</a>
+                </li>
+                <li>
+                  <a href>실시간 기상정보</a>
+                </li>
+                <li>
+                  <a href>오늘/내일/모레 기상정보</a>
+                </li>
+              </MMOList>
+              <MMOContainer>
+                <MMOSelectWrapper>
+                  <MMOSelect bg width="188px">
+                    <option value="KHAI">통합대기환경지수(CAI)</option>
+                    <option value="10008">초미세먼지 (PM-2.5)</option>
+                    <option value="10007">미세먼지 (PM-10)</option>
+                    <option value="10003">오존(O₃)</option>
+                    <option value="10006">이산화질소(NO₂)</option>
+                    <option value="10002">일산화탄소 (CO)</option>
+                    <option value="10001">아황산가스 (SO₂)</option>
+                  </MMOSelect>
+                </MMOSelectWrapper>
+                <MMOSelectWrapper>
+                  <label for="area1" style={{ marginRight: '5px' }}>
+                    시/도
+                  </label>
+                  <MMOSelect bg id="area1" width="130px">
+                    <option value="">-전체-</option>
+                    <option value="02">서울특별시</option>
+                    <option value="051">부산광역시</option>
+                    <option value="053">대구광역시</option>
+                    <option value="032">인천광역시</option>
+                    <option value="062">광주광역시</option>
+                    <option value="042">대전광역시</option>
+                    <option value="052">울산광역시</option>
+                    <option value="031">경기도</option>
+                    <option value="033">강원특별자치도</option>
+                    <option value="043">충청북도</option>
+                    <option value="041">충청남도</option>
+                    <option value="063">전라북도</option>
+                    <option value="061">전라남도</option>
+                    <option value="044">세종특별자치시</option>
+                    <option value="054">경상북도</option>
+                    <option value="055">경상남도</option>
+                    <option value="064">제주특별자치도</option>
+                  </MMOSelect>
+                </MMOSelectWrapper>
+              </MMOContainer>
+            </MMOptionLayout>
+            {/* Main Map 전국 지도 */}
+            <MMWrapper>
+              <svg
+                version="1.1"
+                id="map_svg_city"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                x="0px"
+                y="0px"
+                width="700px"
+                height="730px"
+                viewBox="0 0 700 730"
+                enable-background="new 0 0 700 730"
+                xmlS
+                pace="preserve"
+              >
+                <g>
+                  <IncheonPath />
+                  <SeoulPath />
+                  <GyeonggiPath />
+                  <GangwonPath />
+                  <ChungbukPath />
+                  <ChungnamPath />
+                  <SejongPath />
+                  <DaejeonPath />
+                  <GyeongbukPath />
+                  <GyeongnamPath />
+                  <DaeguPath />
+                  <UlsanPath />
+                  <BusanPath />
+                  <JeonbukPath />
+                  <JeonnamPath />
+                  <GwangjuPath />
+                  <JejuPath />
+                </g>
+              </svg>
+              <Time>
+                0000년 00월 00일 <strong>00시</strong>
+              </Time>
+            </MMWrapper>
+          </MMLayout>
+          {/* 대기/기상 데이터 정보 */}
+          <InfoContainer>
+            <div>
+              <h2>
+                우리동네 <span>대기정보</span>
+              </h2>
+              <div>
+                <button>검색</button>
+                <button>현위치</button>
+                <p>
+                  <strong>중구</strong> 중심으로 측정
+                  <span>(서울 중구 중구 측정소 기준)</span>
+                </p>
+                <Time>
+                  0000년 00월 00일 <strong>00시</strong>{' '}
+                  <button>새로고침</button>
+                </Time>
+              </div>
+            </div>
+          </InfoContainer>
         </FirstSection>
       </Main>
     </>
