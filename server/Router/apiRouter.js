e99deac2d24aa6453a78cd847e4727ac9fa8fe26
hansan529 <hansan0529@gmail.com>
2024-04-04 23:29:56 +0900

    docs: 날씨 정보 api 추가

--- server/Router/apiRouter.js
+++ server/Router/apiRouter.js
@@ -11,18 +11,18 @@ router.post('/coordinate', async (req, res) => {
         return res.json(response);
     }catch(err){
         console.error("error: ", err);
-        return res.end();
+        return res.json(false)();
     }
 })
 
 router.post('/station', async (req, res) => {
     try {
         const { x, y } = req.body;
-        const {response: { body: { items }}} = await (await axios.get(`http://apis.data.go.kr/B552584/MsrstnInfoInqireSvc/getNearbyMsrstnList?serviceKey=${process.env.REACT_APP_AIRKOREA_SERVICEKEY}&returnType=json&tmX=${x}&tmY=${y}&ver=1.1`)).data;
+        const {response: { body: { items }}} = await (await axios.get(`http://apis.data.go.kr/B552584/MsrstnInfoInqireSvc/getNearbyMsrstnList?serviceKey=${process.env.REACT_APP_OPENAPI_SERVICEKEY}&returnType=json&tmX=${x}&tmY=${y}&ver=1.1`)).data;
         return res.json(items)
     } catch(err) {
         console.error("error: ", err);
-        return res.end();
+        return res.json(false)();
     }
 })
 
@@ -43,20 +43,30 @@ router.get('/getMinuDustFrcstDspth', async (req, res) => {
             result = `${year}.${month}.${day}`;
         }
 
-        const {response: {body: { items }}} = await (await axios.get(`http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth?serviceKey=${process.env.REACT_APP_AIRKOREA_SERVICEKEY}&returnType=json&numOfRows=700&pageNo=1&searchDate=${result}`)).data;
+        const {response: {body: { items }}} = await (await axios.get(`http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth?serviceKey=${process.env.REACT_APP_OPENAPI_SERVICEKEY}&returnType=json&numOfRows=700&pageNo=1&searchDate=${result}`)).data;
         return res.json(items);
     } catch(err) {
         console.error("error: ", err);
-        return res.end;
+        return res.json(false);
     }
 });
 router.get('/getCtprvnRltmMesureDnsty', async (req, res) => {
     try {
-        const {response: {body: { items }}} = await (await axios.get(`http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=${process.env.REACT_APP_AIRKOREA_SERVICEKEY}&returnType=json&numOfRows=700&pageNo=1&sidoName=전국&ver=1.0`)).data;
+        const {response: {body: { items }}} = await (await axios.get(`http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=${process.env.REACT_APP_OPENAPI_SERVICEKEY}&returnType=json&numOfRows=700&pageNo=1&sidoName=전국&ver=1.0`)).data;
         return res.json(items);
     } catch(err) {
         console.error("error: ", err);
-        return res.end;
+        return res.json(false);
+    }
+});
+
+router.get('/getWthrSituation', async (req, res) => {
+    try {
+        const {response: {body: { items }}} = await (await axios.get(`http://apis.data.go.kr/1360000/VilageFcstMsgService/getWthrSituation?ServiceKey=${process.env.REACT_APP_OPENAPI_SERVICEKEY}&pageNo=1&numOfRows=1&dataType=JSON&stnId=108`)).data
+        return res.json(items);
+    }catch(err) {
+        console.error('error: ', err);;
+        return res.json(false);
     }
 })
 
