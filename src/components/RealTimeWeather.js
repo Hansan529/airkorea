01f5505197e16cb4011c3080d9fb1ca33b2aa0b0
hansan529 <hansan0529@gmail.com>
2024-04-05 23:50:35 +0900

    docs: 날씨 정보 API 받아오기

--- src/components/RealTimeWeather.js
+++ src/components/RealTimeWeather.js
@@ -1,5 +1,23 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export const RealTimeWeather = () => {
+    const [weatherData, setWeatherData] = useState(null);
+
+    useEffect(() => {
+        if(!weatherData){
+            (async () => {
+                const {item} = await (await axios.get('http://localhost:3500/api/getWthrSituation')).data;
+                const [data] = item;
+                setWeatherData(data);
+                localStorage.setItem('weather', JSON.stringify(data));
+            })();
+        }
+    }, [weatherData]);
+
+
     return (
-        <div></div>
+        <div>
+        </div>
     )
 }
