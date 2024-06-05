fbbddc78d2f8147418c15e129d52075a84cc6fdc
hansan529 <hansan0529@gmail.com>
2024-06-05 19:50:56 +0900

    Modify: 로딩 이미지 경로 수정

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -24,7 +24,7 @@ const LoadingScreen = () => {
         <Loading loading={String(loading)}>
             <div>데이터 처리중입니다.</div>
             <p>잠시만 기다려 주시기 바랍니다.</p>
-            <img src="/loading_1.webp" alt="로딩 중" />
+            <img src="/images/global/loading_1.webp" alt="로딩 중" />
         </Loading>
     )
 };
